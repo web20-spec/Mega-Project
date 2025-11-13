d0fcf9db77e6823a21e27a5d6592dbe3cc364328
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-11-13 13:53:52 -0800

    More components

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React, { Children } from 'react'
+
+function Button({
+    children,
+    type = 'button',
+    bgColor = 'bg-blue-600',
+    textColor = 'text-white',
+    className = '',
+    ...props 
+}) {
+  return (
+    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}></button>
+  )
+}
+
+export default Button
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container.jsx"
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
