383eb19a08efaca7cc48af7c52adba0fd3f7b5ff
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-11-17 14:33:19 -0800

    continued...

---
+++ src/components/Signup.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import authService from '../appwrite/auth'
+import {Link, useNavigate} from 'react-router-dom'
+import {login} from '../store/authSlice'
+import {Button, Input, Logo} from './index'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+
+function Signup() {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const [error, setError] = useState("")
+    const {register, handleSubmit} = useForm()
+
+    const create = async(data) => {
+        setError("")
+        try {
+            
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
