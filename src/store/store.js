16c79e8c01a0f27db6e09f00d07a0918fc62ad06
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-11-19 14:55:25 -0800

    Pages

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
+import { login, logout } from './authSlice'
 
 const store = configureStore({
-    reducer:{}
+    reducer:{
+        login,
+        logout
+    }
 })
 
 export default store
